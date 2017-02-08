287571289f24f6cf4426e796cb79d77ac4d2784e
ingadd23 <ingadd23@gmail.com>
2017-02-08 19:39:51 +0100

    v3

--- 2/js/javascript.js
+++ 2/js/javascript.js
@@ -22,8 +22,52 @@ $(function(){
 	function getCurrentScroll() {
 		return window.pageYOffset || document.documentElement.scrollTop;
 	}
-	
-	var shiftWindow = function() { scrollBy(0, -50) };
-if (location.hash) shiftWindow();
-window.addEventListener("hashchange", shiftWindow);
 });
+
+
+$(function(){
+  $(".navbar a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+		$('#menu').removeClass('in'); // close right menu after click
+		if(window.innerWidth > 768){ // screen width
+			$('html, body').animate({
+			scrollTop: $(hash).offset().top-79 // top margin
+		  }, 900, function(){
+			window.location.hash = hash;
+
+		  });
+		}
+		else {
+			$('html, body').animate({
+			scrollTop: $(hash).offset().top-50
+		  }, 900, function(){
+			window.location.hash = hash;
+
+		  });
+		}
+    }
+  });
+
+});
+
+function viewMoreGallery(){
+	document.getElementById("more").style.display = "block";
+	document.getElementById("gallery_button").style.display = "none";
+	$(".more > div  > a ").attr("data-toggle", "lightbox").attr("data-gallery", "gallery");
+}
+
+function viewMoreBlog(){
+	document.getElementById("more_blog").style.display = "block";
+	document.getElementById("blog_button").style.display = "none";
+}
+
+
+(function($) {
+  $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+  });
+})(jQuery); 
+
